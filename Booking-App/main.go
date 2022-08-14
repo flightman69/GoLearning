944aa1a440d61b64eef2e38985db4218320c0145
flightman69 <flightman69420@gmail.com>
2022-08-14 17:39:54 +0530

    update

--- Booking-App/main.go
+++ Booking-App/main.go
@@ -1,20 +1,89 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+// Show Details
+var showName string = "The Lan Party"
+
+const tot_tickets uint = 50
+
+var remainingTickets = tot_tickets
+var bookings = []string{}
 
 func main() {
-	show_name := "Go Show"
-	const tot_show_tickets uint = 50
-	var remaining_tickets = 50
-	fmt.Printf("tot_show_tickets is %T, show_name is a %T\n", tot_show_tickets, show_name)
-	fmt.Printf("Welcome to %v booking application\n", show_name)
-	fmt.Println("Get your tickets here to attend! Only", remaining_tickets, "tickets left")
 
-	var user_name string
-	var user_tickets int
-	// ask user for their name
+	for {
+		//Greet Users
+		greetUsers()
 
-	user_name = "Mike"
-	user_tickets = 3
-	fmt.Printf("User %v booked %v tickets\n", user_name, user_tickets)
+		//Get User Input
+		userName, userTickets := getUserInput()
+		if userTickets > remainingTickets || userTickets == 0 {
+			fmt.Printf("Invalid amount of tickets only %v tickets left\n", remainingTickets)
+			continue
+		}
+		fmt.Printf("Thank you %v for buying %v tickets\nHave a Great day :)\n\n", userName, userTickets)
+
+		//Update Booking and remaining details
+		bookings, remainingTickets = bookingUpdate(bookings, remainingTickets, userName, userTickets)
+
+		//Checks if all tickets sold out
+		if remainingTickets == 0 {
+			//end of program
+			fmt.Print("All tickets sold out :(\n\n")
+			fmt.Print("===============================================================\n\n")
+			break
+		}
+	}
+
+}
+
+func greetUsers() {
+	fmt.Print("===============================================================\n")
+	fmt.Printf("\t\tWelcome to the %v \n\t\tYou can buy your tickets here\n\t\tOnly %v tickets remaining!\n", showName, remainingTickets)
+	fmt.Print("===============================================================\n\n")
+
+}
+
+func getUserInput() (string, uint) {
+
+	fmt.Println("What's your name ?")
+	var userName string
+	fmt.Scanf("%v", &userName)
+
+	fmt.Printf("Hello %v how many tickets you like to buy ??\n", userName)
+	var userTickets uint
+	fmt.Scan(&userTickets)
+
+	return userName, userTickets
+
+}
+
+//geto booking details
+
+func getBookingDetails(totBookings []string) []string {
+	firstNames := []string{}
+	for _, names := range totBookings {
+		var first_name = strings.Split(names, "_")
+		firstNames = append(firstNames, first_name[0])
+	}
+
+	return firstNames
+
+}
+
+func bookingUpdate(bookings []string, remaining_tickets uint, userName string, userTickets uint) ([]string, uint) {
+	//Updating booking and tickets details
+	bookings = append(bookings, userName)
+	remaining_tickets = remaining_tickets - userTickets
+
+	//Print booking details
+	bookingDetails := getBookingDetails(bookings)
+	fmt.Printf("Total Bookings %v and now %v tickets are remaining\n", bookingDetails, remaining_tickets)
+	fmt.Print("===============================================================\n\n")
+
+	return bookings, remaining_tickets
 }
